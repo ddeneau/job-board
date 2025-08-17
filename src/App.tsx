8ae4183aef1a830dd8e67c794406a34fbab4f5db
Dan <daniel.a.deneau@gmail.com>
2025-08-17 19:40:47 -0400

    Revert "Tweaked navigation. Re-added Authentication."
    This reverts commit 5ef9d019cd048e4181d3c83ea61c994e638c490c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import Example from './Example'; 
 import Home from './home/Home';  
-import Users from './users/Users';
-import Profile from './profile/Profile';    
 import Navigation from './navigation/Navigation';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
-import { Authenticator } from '@aws-amplify/ui-react';
+
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         
     <Route path="/" element={<Navigation />}>
-            <Route index element={<Home />} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/board" element={<Example/>} />
-            <Route path="/users" element={<Users />} />
-
-
-            <Route path="/profile" element={
-                <Authenticator>
-                    <Profile/>
-                </Authenticator>} />
+            <Route index element={<Example />} />
+            <Route path="/home" element={<Home />}/>
     </Route>
 
     )
